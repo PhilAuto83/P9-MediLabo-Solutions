b5b57a02b16add33872a669c36fc96799fd04910
Philip Mercan <philmercan@gmail.com>
2024-07-30 16:08:28 +0200

    adding test on diabete controller to finalize microservice mls-diabete

--- mls-diabete/src/main/java/com/phildev/mls/diabete/exception/RestExceptionHander.java
+++ mls-diabete/src/main/java/com/phildev/mls/diabete/exception/RestExceptionHander.java
@@ -4,11 +4,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
+@RestControllerAdvice
 public class RestExceptionHander {
 
 
